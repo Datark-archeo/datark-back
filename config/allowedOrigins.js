211d5258adf3a42919c91acba5034f4972d742c9
Nogaruki <47275430+nogaruki@users.noreply.github.com>
2024-03-27 09:56:59 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
     'https://www.datark.com',
     'http://127.0.0.1:4200',
-    'http://localhost:4200'
-
+    'http://localhost:4200',
+    'https://datark-front.onrender.com'
 ];
 
 module.exports = allowedOrigins;
