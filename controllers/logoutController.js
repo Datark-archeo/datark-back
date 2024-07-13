99281284b08f4cd5d3b1383aeb6276efe64362e6
Johann <ryukjojo@hotmail.fr>
2024-07-13 15:52:10 +0200

    FIX logout crash

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -23,22 +23,36 @@ const User = require('../models/user.model');
  */
 const handleLogout = async (req, res) => {
     const cookies = req.cookies;
-    if (!cookies?.jwt) return res.sendStatus(204); //No content
+    if (!cookies?.jwt) return res.sendStatus(204); // No content
     const refreshToken = cookies.jwt;
 
-    // Is refreshToken in db?
-    const foundUser = await User.findOne({ refreshToken: refreshToken }).exec();
-    if (!foundUser) {
-        res.clearCookie('jwt', { httpOnly: true, secure: true });
-        return res.sendStatus(204);
+    try {
+        // Is refreshToken in db?
+        const foundUser = await User.findOne({ refreshToken: refreshToken }).exec();
+        if (!foundUser) {
+            res.clearCookie('jwt', { httpOnly: true, secure: true });
+            return res.sendStatus(204); // No content
+        }
+
+        // Delete refreshToken in db
+        foundUser.refreshToken = foundUser.refreshToken.filter(rt => rt !== refreshToken);
+
+        try {
+            await foundUser.save();
+            res.clearCookie('jwt', { httpOnly: true, secure: true });
+            res.sendStatus(204); // No content
+        } catch (error) {
+            if (error instanceof mongoose.Error.VersionError) {
+                // Handle VersionError
+                res.status(409).send({ message: 'Document has been modified, please try again' });
+            } else {
+                // Handle other errors
+                res.status(500).send({ message: 'Internal server error' });
+            }
+        }
+    } catch (error) {
+        res.status(500).send({ message: 'Internal server error' });
     }
-
-    // Delete refreshToken in db
-    foundUser.refreshToken = foundUser.refreshToken.filter(rt => rt !== refreshToken);
-    const result = await foundUser.save();
-
-    res.clearCookie('jwt', { httpOnly: true, secure: true });
-    res.sendStatus(204);
 }
 
-module.exports = { handleLogout }
+module.exports = { handleLogout };
