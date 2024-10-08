9ac7cfabfa273d5f9cfba99fbd50fa107d5596ee
Nogaruki <johann.av.pro@outlook.fr>
2024-10-08 21:15:19 +0200

    fix error get 404

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 
 // Serve static files
-app.use('/uploads', express.static(join(__dirname, 'uploads')));
+app.use('/api/uploads', express.static(join(__dirname, 'uploads')));
 
 if (!process.env.ACCESS_TOKEN_SECRET) {
     throw new Error('ACCESS_TOKEN_SECRET is not defined');
