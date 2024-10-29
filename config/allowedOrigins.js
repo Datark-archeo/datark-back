258565feb5893f20c5e2c70da5d6af2e4f0f60e1
Nogaruki <johann.avramov@datark.fr>
2024-10-29 12:20:33 +0000

    adding origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://www.datark.com',
     'http://localhost:4200',
-    'https://preprod.datark.fr'
+    'https://preprod.datark.fr',
+    'https://www.preprod.datark.fr'
 ];
 
 module.exports = allowedOrigins;
